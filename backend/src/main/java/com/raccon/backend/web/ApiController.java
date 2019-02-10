92fb225b94afe928d564b7c56620a602102aba30
Ilya Matveev <ilmat192@gmail.com>
2019-02-10 15:25:23 +0300

    Hotfix: reject long expressions

--- backend/src/main/java/com/raccon/backend/web/ApiController.java
+++ backend/src/main/java/com/raccon/backend/web/ApiController.java
@@ -5,11 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SuppressWarnings("unused")
 @Validated
 @RestController
 @CrossOrigin(maxAge = 3600, methods = {RequestMethod.GET, RequestMethod.POST})
 public class ApiController {
+
+    static private int EXPRESSION_LENGTH_THRESHOLD = 10000;
+
     @Autowired
     private EvaluatorService evaluator;
 
@@ -21,14 +28,36 @@ public class ApiController {
         return new Response<>(Response.Status.HEALTH_CHECK_ETO_HOROSHECHNO, result, message);
     }
 
+    // TODO: It's just a hotfix. Remove this when the attack stops.
+    private boolean isTooLong(String expression) {
+        return expression.length() > EXPRESSION_LENGTH_THRESHOLD;
+    }
+
+    private void logExpression(String expression) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss ");
+        Date date = new Date();
+        System.out.print(dateFormat.format(date));
+        System.out.print("expression: ");
+        System.out.println(expression);
+    }
+
     @GetMapping("/evaluate")
     @ResponseBody
     public Response<Double> evaluate(@RequestParam("expression") String expression) {
+        logExpression(expression);
+        if (isTooLong(expression)) {
+            return new Response<>(Response.Status.FAIL, 0.0, "Expression is too long!");
+        }
+
         return new Response<>(Response.Status.OK, evaluator.evaluate(expression));
     }
 
     @PostMapping("/evaluate")
     public Response<Double> evaluate(@RequestBody Request request) {
+        logExpression(request.getExpression());
+        if (isTooLong(request.getExpression())) {
+            return new Response<>(Response.Status.FAIL, 0.0, "Expression is too long!");
+        }
         return new Response<>(Response.Status.OK, evaluator.evaluate(request.getExpression()));
     }
 
